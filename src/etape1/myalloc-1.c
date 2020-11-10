495724583008f615ac9d954bd0122433c57f40e2
Tom-Rey <60466038+Tom-Rey@users.noreply.github.com>
2020-11-10 11:02:37 +0100

    Update myalloc-1.c

--- src/etape1/myalloc-1.c
+++ src/etape1/myalloc-1.c
@@ -2,9 +2,6 @@
 #include "myalloc-bloc-entete-0.h"
 #include <stdio.h>
 
-#include "myalloc-1.c.inc"
-#include "myfree-1.c.inc"
-#include "blocinfo-1.c.inc"
 
 void* myalloc(size_t n){
 	size_t taille=ALIGN(n+ENTETE_SIZE);
