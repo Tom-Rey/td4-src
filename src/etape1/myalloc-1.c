b471a519b43e89a9c13373d3647a46aed89fdfdb
Tom-Rey <60466038+Tom-Rey@users.noreply.github.com>
2020-11-10 10:40:30 +0100

    Update myalloc-1.c

--- src/etape1/myalloc-1.c
+++ src/etape1/myalloc-1.c
@@ -1,10 +1,30 @@
 #include "../generic/myalloc.h"
-#include "myalloc-bloc-entete-1.h"
+#include "myalloc-bloc-entete-0.h"
 #include <stdio.h>
 
 
+void* myalloc(size_t n){
+	size_t taille=ALIGN(n+ENTETE_SIZE);
+	void* ptr=(void*) sbrk(taille);
 
+	bloc_entete* ptr_entete=(bloc_entete*) ptr;
+	ptr-entete->taille = taille;
+	ptr-entete->libre=0;
 
-#include "myalloc-1.c.inc"
-#include "myfree-1.c.inc"
-#include "blocinfo-1.c.inc"
+	void* user_addresse = ((char*) ptr)+ENTETE_SIZE;
+	return user_addresse;
+}
+void myfree(void* user_addresse){
+	bloc_entete* ptr-entete= (bloc_entete*) ((char*)user_addresse - ENTETE_SIZE);
+	
+	ptr-entete->libre=1;
+}
+
+void blocinfo(void* ptr){
+	bloc_entete* bloc_tot=(bloc_entete*) ((char*)ptr - ENTETE_SIZE);
+	printf("pointeur bloc%p \n, pointeur donnees %p \n, taille totale: %d, \nbloc libre? %d\n",
+	bloc_tot,
+	ptr
+	bloc_tot->taille,
+	bloc_tot->libre);
+}
