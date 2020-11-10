d770ac8b64c38cbceeec470b6f508d4bd18ba9dc
Tom-Rey <60466038+Tom-Rey@users.noreply.github.com>
2020-11-10 10:48:45 +0100

    Update myalloc-1.c

--- src/etape1/myalloc-1.c
+++ src/etape1/myalloc-1.c
@@ -24,10 +24,10 @@ void myfree(void* user_addresse){
 }
 
 void blocinfo(void* ptr){
-	bloc_entete* bloc_tot=(bloc_entete*) ((char*)ptr - ENTETE_SIZE);
+	bloc_entete* bloc_total=(bloc_entete*) ((char*)ptr - ENTETE_SIZE);
 	printf("pointeur bloc%p \n, pointeur donnees %p \n, taille totale: %d, \nbloc libre? %d\n",
-	bloc_tot,
+	bloc_total,
 	ptr
-	bloc_tot->taille,
-	bloc_tot->libre);
+	bloc_total->taille,
+	bloc_total->libre);
 }
