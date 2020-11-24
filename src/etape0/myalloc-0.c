408adff9a7e25c00626ba34afc4e77737d0627bd
Tom-Rey <60466038+Tom-Rey@users.noreply.github.com>
2020-11-24 09:39:21 +0100

    Update myalloc-0.c

--- src/etape0/myalloc-0.c
+++ src/etape0/myalloc-0.c
@@ -3,25 +3,21 @@
 #include <stdio.h>
 
 
-#include "myalloc-0.c.inc"
-#include "myfree-0.c.inc"
-#include "blocinfo-0.c.inc"
-
 void* myalloc(size_t n){
 	size_t taille=ALIGN(n+ENTETE_SIZE);
 	void* ptr=(void*) sbrk(taille);
 
 	bloc_entete* ptr_entete=(bloc_entete*) ptr;
-	ptr-entete->taille = taille;
-	ptr-entete->libre=0;
+	ptrentete->taille = taille;
+	ptrentete->libre=0;
 
 	void* user_addresse = ((char*) ptr)+ENTETE_SIZE;
 	return user_addresse;
 }
 void myfree(void* user_addresse){
-	bloc_entete* ptr-entete= (bloc_entete*) ((char*)user_addresse - ENTETE_SIZE);
+	bloc_entete* ptrentete= (bloc_entete*) ((char*)user_addresse - ENTETE_SIZE);
 	
-	ptr-entete->libre=1;
+	ptrentete->libre=1;
 }
 
 void blocinfo(void* ptr){
