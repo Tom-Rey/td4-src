bd85516163780aa975d1ee01d63ea3a378ff63a6
Tom-Rey <60466038+Tom-Rey@users.noreply.github.com>
2020-11-30 22:51:11 +0100

    Update myalloc-1.c

--- src/etape1/myalloc-1.c
+++ src/etape1/myalloc-1.c
@@ -1,30 +1,31 @@
 #include "../generic/myalloc.h"
 #include "myalloc-bloc-entete-0.h"
 #include <stdio.h>
+#include <unistd.h>
 
 
 void* myalloc(size_t n){
 	size_t taille=ALIGN(n+ENTETE_SIZE);
-	void* ptr=(void*) sbrk(taille);
-
+	void* ptr= (void*) sbrk(taille);
+	
 	bloc_entete* ptr_entete=(bloc_entete*) ptr;
-	ptr-entete->taille = taille;
-	ptr-entete->libre=0;
+	ptr_entete->taille = taille;
+	ptr_entete->libre=0;
 
 	void* user_addresse = ((char*) ptr)+ENTETE_SIZE;
 	return user_addresse;
 }
 void myfree(void* user_addresse){
-	bloc_entete* ptr-entete= (bloc_entete*) ((char*)user_addresse - ENTETE_SIZE);
+	bloc_entete* ptr_entete= (bloc_entete*) ((char*)user_addresse - ENTETE_SIZE);
 	
-	ptr-entete->libre=1;
+	ptr_entete->libre=1;
 }
 
-void blocinfo(void* ptr){
-	bloc_entete* bloc_total=(bloc_entete*) ((char*)ptr - ENTETE_SIZE);
+void bloc_info(void* ptr){
+	bloc_entete* bloc_tot=(bloc_entete*) ((char*)ptr - ENTETE_SIZE);
 	printf("pointeur bloc%p \n, pointeur donnees %p \n, taille totale: %d, \nbloc libre? %d\n",
-	bloc_total,
-	ptr
-	bloc_total->taille,
-	bloc_total->libre);
+	bloc_tot,
+	ptr,
+	bloc_tot->taille,
+	bloc_tot->libre);
 }
